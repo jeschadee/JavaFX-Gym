2f1743b1312434ec5b3c595ef537483e6ffbf027
Jesus Colmenares <jeschadee@gmail.com>
2023-02-05 20:04:28 -0300

    primer commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module app.javafxgym {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens app.javafxgym to javafx.fxml;
+    exports app.javafxgym;
+}
