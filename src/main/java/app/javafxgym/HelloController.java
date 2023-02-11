916a935ca44dd432a8311e17c98625464937198e
Agustin Schulze <aguschuger@hotmail.com>
2023-02-11 01:55:26 -0300

    Cambios generales

--- src/main/java/app/javafxgym/HelloController.java
+++ src/main/java/app/javafxgym/HelloController.java
@@ -1,14 +1,41 @@
 package app.javafxgym;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button close;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField username;
+
+    public void close(){
+        System.exit(0);
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
